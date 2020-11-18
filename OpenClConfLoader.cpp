7c596f68bea9472605b347b6c54f517403a89bcb
Nicholas Corgan <n.corgan@gmail.com>
2020-11-18 07:10:54 -0600

    Should be ready to test

--- OpenClConfLoader.cpp
+++ OpenClConfLoader.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 Nicholas Corgan
 // SPDX-License-Identifier: BSL-1.0
 
+#include <Pothos/Callable.hpp>
 #include <Pothos/Exception.hpp>
 #include <Pothos/Framework.hpp>
 #include <Pothos/Object.hpp>
@@ -12,6 +13,7 @@
 #include <Poco/Path.h>
 #include <Poco/StringTokenizer.h>
 
+#include <algorithm>
 #include <map>
 #include <memory>
 #include <string>
@@ -43,58 +45,78 @@ static Pothos::Proxy openClBlockFactory(
     return openClBlock;
 }
 
+static inline std::vector<std::string> stringTokenizerToVector(const Poco::StringTokenizer& tokenizer)
+{
+    std::vector<std::string> stdVector;
+    std::transform(
+        tokenizer.begin(),
+        tokenizer.end(),
+        std::back_inserter(stdVector),
+        [](const std::string& str){return str;});
+
+    return stdVector;
+}
+
 static std::vector<Pothos::PluginPath> OpenClConfLoader(const std::map<std::string, std::string>& config)
 {
-    std::vector<Pothos::PluginPath> entries;
     static const auto tokOptions = Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM;
     static const std::string tokSep(" \t");
     
-    // Set by caller
+    // Set by calling function
     const auto confFilePathIter = config.find("confFilePath");
     if(confFilePathIter == config.end()) throw Pothos::Exception("No conf filepath");
     const auto rootDir = Poco::Path(confFilePathIter->second).makeParent();
 
-    // TODO: sources
+    //
+    // Source must be a path
+    //
+    std::string absSource;
     auto sourceIter = config.find("source");
     if(sourceIter == config.end()) throw Pothos::Exception("No source");
+    absSource = Poco::Path(rootDir, sourceIter->second).toString();
+    if(!Poco::File(absSource).exists())
+    {
+        throw Pothos::FileNotFoundException(absSource);
+    }
 
     auto kernelNameIter = config.find("kernel_name");
     if(kernelNameIter == config.end()) throw Pothos::Exception("No kernel name");
 
-    std::vector<Pothos::PluginPath> factories;
-    std::vector<std::string> docSources;
+    auto inputTypesIter = config.find("input_types");
+    if(inputTypesIter == config.end()) throw Pothos::Exception("No input types");
+    auto inputTypes = stringTokenizerToVector(Poco::StringTokenizer(inputTypesIter->second, tokSep, tokOptions));
 
-    auto docSourcesIter = config.find("doc_sources");
-    if(docSourcesIter != config.end())
-    {
-        for(const auto& docSource: Poco::StringTokenizer(docSourcesIter->second, tokSep, tokOptions))
-        {
-            const auto absPath = Poco::Path(docSource).makeAbsolute(rootDir);
-            docSources.push_back(absPath.toString());
-        }
-    }
-    else
-    {
-        const auto absPath = Poco::Path(sourceIter->second).makeAbsolute(rootDir);
-        docSources.push_back(absPath.toString());
-    }
-
-    // TODO: test compiling source outside block
+    auto outputTypesIter = config.find("output_types");
+    if(outputTypesIter == config.end()) throw Pothos::Exception("No output types");
+    auto outputTypes = stringTokenizerToVector(Poco::StringTokenizer(outputTypesIter->second, tokSep, tokOptions));
 
     //
     // Generate and store JSON block docs
     //
     Pothos::Util::BlockDescriptionParser parser;
-    for(const auto& source: docSources) parser.feedFilePath(source);
-    for(const auto& factory: parser.listFactories())
+    parser.feedFilePath(absSource);
+
+    const auto& factories = parser.listFactories();
+    assert(factories.size() == 1);
+
+    Pothos::PluginPath pluginPath = Pothos::PluginPath("/blocks/docs", pluginPath);
+    Pothos::PluginRegistry::add(pluginPath, parser.getJSONObject(factories[0]));
+
+    //
+    // Register all factory paths, using the parameters from the config file.
+    //
+    OpenClBlockArgs blockArgs =
     {
-        const auto pluginPath = Pothos::PluginPath("/blocks/docs", factory);
-        Pothos::PluginRegistry::add(pluginPath, parser.getJSONObject(factory));
+        absSource,
+        kernelNameIter->second,
+        inputTypes,
+        outputTypes
+    };
 
-        factories.emplace_back("/blocks", factory);
-    }
+    auto blockFactory = Pothos::Callable(&openClBlockFactory).bind(blockArgs, 2);
+    Pothos::PluginRegistry::addCall(pluginPath, blockFactory);
 
-    return entries;
+    return std::vector<Pothos::PluginPath>{pluginPath};
 }
 
 //
